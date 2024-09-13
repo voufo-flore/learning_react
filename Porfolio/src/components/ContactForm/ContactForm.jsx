ce17f988f8377314babda3313a51193c12c12531
voufo-flore <floresvoufo@gmail.com>
2024-09-13 15:33:25 +0100

    dashboard

--- Porfolio/src/components/ContactForm/ContactForm.jsx
+++ Porfolio/src/components/ContactForm/ContactForm.jsx
@@ -3,8 +3,17 @@ import "./ContactForm.css"
 
 const ContactForm = () => {
   return (
-    <div className='contact'>
-      <p>VOUFO FLORE</p>
+    <div className='contact-form-content'>
+        <form>
+            <div className='name-container'>
+                <input type="text" name='firstname' placeholder='VOUFO'/>
+                <input type="text" name='lastname' placeholder='FLORE'/>
+            </div>
+            <input type="text"  name='email' placeholder='florevoufo@gmail.com'/>
+            <input type="text"  name='contact' placeholder='+237654897023'/>
+            <textarea name="text" type="Message" placeholder='Message' rows={3}></textarea>
+            <button>SEND</button>
+        </form>
     </div>
   );
 };
